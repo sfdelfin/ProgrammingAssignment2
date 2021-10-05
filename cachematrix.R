200300239eb47830466ff8383ee7ff8e7917ebd1
sfdelfin <91895595+sfdelfin@users.noreply.github.com>
2021-10-05 11:01:48 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Lexical Scoping
 
-## Write a short comment describing this function
+## Makes special matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+# var and functions
+        matrizinversa <- NULL
+        set <- function( y ){
+                x <<- y
+                matrizinversa <<- NULL
+                }
+                get <- function() ( x )
+        setInversa <- function( calculoinversa ) ( matrizinversa <<- calculoinversa )
+        getInversa <- function() ( matrizinversa )
+        list( set = set, get = get, setInversa = setInversa, getInversa = getInversa )
 }
 
 
-## Write a short comment describing this function
+## Computes the inverse of the special matrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        solinversa <- x$getInversa()
+        if (!is.null(solinversa)) {
+                 message( "getting cached data" )
+                 return( solinversa )
+                 }
+        data <- x$get()
+        solinversa <- solve(data, ...)
+        x$setInversa(solinversa)
+        solinversa
 }
